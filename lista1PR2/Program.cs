1cd6614864c8d00b90e2cbfdf78b8037d9994bf4
CJ3017095 <CJ3017095@LAB01-PC18>
2023-03-03 10:29:13 -0300

    Adicionar arquivos de projeto.

---
+++ lista1PR2/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1PR2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //   int n, cent, mili;
+            //Console.Write("digite um número de A: ");
+            //n = int.Parse(Console.ReadLine());
+            //   cent = (n * 100);
+            //   mili = (n * 1000);
+
+
+            //Console.WriteLine("os centimetros são " + cent);
+            //Console.WriteLine("os milimetros são " + mili);
+
+
+            //      Console.ReadKey();
+            // 1 - TRABALHO DOS CENTIMETROS E MILIMETROS
+
+            //float cel, fah;
+            //Console.WriteLine("quantos graus fahrenheit ?");
+            //fah = float.Parse(Console.ReadLine());
+
+            //cel = (fah - 32.0f) * (5.0f / 9.0f);
+
+            //Console.WriteLine("os graus são " + cel);
+
+            //Console.ReadKey();
+
+            //2-exercicio dos graus A
+
+            //int cel, fah;
+            //Console.WriteLine("quantos graus fahrenheit ?");
+            //fah = int.Parse(Console.ReadLine());
+
+            //cel = (fah - 32) * (5 / 9);
+
+            //Console.WriteLine("os graus são " + cel);
+
+            //Console.ReadKey();
+
+            //2- exercicio dos graus B 
+            //resposta: todo numero inteiro dividido por inteiro da um resultado inteiro
+
+            //float alt, peso,imc;
+            //Console.WriteLine("qual sua altura ?");
+            //alt = float.Parse(Console.ReadLine());
+            //Console.WriteLine( "qual seu peso ? ");
+            //peso = float.Parse(Console.ReadLine());
+
+            //imc = peso / (alt*alt);
+            //Console.WriteLine("seu imc é " + imc);
+            //Console.ReadKey();
+            // 3- formula do IMC
+
+            //float n1, n2, n3, peso1, peso2, peso3, media;
+
+            //Console.WriteLine("qual o valor do primeiro numero? ");
+            //n1 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("qual o valor do segundo numero? ");
+            //n2 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("qual o valor do terceiro numero? ");
+            //n3 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("qual o valor do primeiro peso? ");
+            //peso1 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("qual o valor segundo peso? ");
+            //peso2 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("qual o valor do terceiro peso? ");
+            //peso3 = float.Parse(Console.ReadLine());
+
+            //media = (peso1 + peso2 + peso3)/(n1 + n2 + n3);
+            //Console.WriteLine("a media ponderada é de " + media);
+            //Console.ReadKey();
+            //4- media ponderada 
+
+            // float acel, velo, temp;
+            // Console.WriteLine("qual a velocidade? ");
+            // velo = float.Parse(Console.ReadLine());
+            // Console.WriteLine("qual a tempo? ");
+            // temp = float.Parse(Console.ReadLine());
+
+            // acel = velo  / temp;    
+
+            //Console.WriteLine("a aceleração é " + acel + " metros por segundo");
+            // Console.ReadKey();
+            //5- formula de fisica
+
+            //float sala, resul, porc;
+            //Console.WriteLine("qual o salario?");
+            //sala = float.Parse(Console.ReadLine()); 
+
+            //porc = sala * 0.25f;
+            //resul= porc+sala;
+            //Console.WriteLine("o salario será :" + resul);
+
+            //Console.ReadKey(); 
+            // 6-porcentagem do salario 
+
+
+            //float real, dolar;
+            //Console.WriteLine("quantos você quer converter ?");
+            //real = float.Parse(Console.ReadLine());
+
+            //dolar = (real / 5);
+            //Console.WriteLine(" a conversão deu:" + dolar);
+            //Console.ReadKey();  
+            //7- conversao de dolar
+
+            //int n, tn, dn, rn;
+            //Console.WriteLine("insira o numero desejado :");
+            //n = int.Parse(Console.ReadLine());
+            //rn = (n * 3 + 1) + (n * 2 -1 );
+            //Console.WriteLine("o valor é "+ rn);
+            //Console.ReadKey();
+            //8- atividade 8
+
+        }
+    }
+}
